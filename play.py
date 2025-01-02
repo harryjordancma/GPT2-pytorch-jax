d7f788b42af4ab7e6bb9622f652e5ae772ac2f3b
HarryJordan <>
2025-01-02 18:32:33 +0000

    Started

---
+++ play.py
@@ -0,0 +1,47 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3.11 Transformers
+#     language: python
+#     name: py311_transformers
+# ---
+
+# %%
+from transformers import GPT2LMHeadModel
+
+# %% jupyter={"outputs_hidden": true}
+model_hf = GPT2LMHeadModel.from_pretrained("gpt2") # 124M
+sd_hf = model_hf.state_dict()
+
+for k, v in sd_hf.items():
+    print(k, v.shape)
+
+# %%
+import matplotlib.pyplot as plt
+
+plt.imshow(sd_hf["transformer.wpe.weight"], cmap="gray")
+
+# %%
+plt.plot(sd_hf["transformer.wpe.weight"][:, 50])
+plt.plot(sd_hf["transformer.wpe.weight"][:, 100])
+plt.plot(sd_hf["transformer.wpe.weight"][:, 200])
+plt.xlabel("embedding dimension")
+plt.ylabel("weights")
+
+# %%
+plt.imshow(sd_hf["transformer.h.1.attn.c_attn.weight"][:300,:300], cmap="gray")
+
+# %%
+from transformers import pipeline, set_seed
+generator = pipeline("text-generation", model="gpt2")
+
+set_seed(42)
+generator("Hello, I'm a large language model", max_length=30, num_return_sequences=5)
+
+# %%
---
+++ pytorch/train_gpt2.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate = "tanh")
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 6
+    n_head: int = 6
+    n_embd: int = 384
+
+class GPT(nn.module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        # nn.ModuleDict, allows us to access the submodules with keys 
+        # wte (weights of token embeddings)
+        # wpe (weights of the position embeddings)
+        # h (hidden layers), a modulelist that we can access with int.
+        # ln_f (layer normal final)
+        # lm_head (language model head, projects to vocab size (50257))
+        # no bias, therefore y = mx + 0
+         self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        

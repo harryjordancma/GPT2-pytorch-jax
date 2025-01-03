7868b8976be0f139d37075fbe0894f6ce1209c24
HarryJordan <>
2025-01-03 16:37:20 +0000

    Finihsed GPT-2 implementation

--- pytorch/train_gpt2.py
+++ pytorch/train_gpt2.py
@@ -20,29 +20,54 @@ class CausalSelfAttention(nn.module):
         self.n_embd = config.n_embd
 
         # Adding a mask (called a bias in the OpenAI/HF naming though
-        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+        # .register_buffer: registers the mask as a buffer, so its not treated as a trainable parameter
+        # torch.ones: create tensor of ones
+        # torch.trill: apply a lower triangle operation
+        # [[1 1 1],   [[1 0 0]
+        #  [1 1 1], -> [1 1 0]
+        #  [1 1 1]]    [1 1 1]]
+        # torch.view: convert the same data to a different shape
+        self.register_buffer("bias", 
+                             torch.tril(torch.ones(config.block_size, config.block_size))
                              .view(1, 1, config.block_size, config.block_size))
 
-    def forward(self, x ):
+    def forward(self, x):
         """
-        Method of the self attention
+        Method of multi-head self attention in a single pass
         """
+        # x is a tensor of shape: 
+        # - Batch size 
+        # - Tokens (sequence length)
+        # - embedding dimension (not sure why its called C))
         B, T, C = x.size()
-        qkv = self.c_attn(x)
-        q, k, v = self.c_attn(x)
+        qkv = self.c_attn(x) # Create query, key, value vectors via projecting linear layer
+        q, k, v = qkv.split(self.n_embd, dim=2) # torch.split: split tensor by which dimension
 
+        """
+        Efficient pass
+        - Making number of heads into a batch dimension so pytorhc treats B and n_heads as batches.
+        - Applies all operations on them in parallel. 
+        """
+        # Shaping q, v, t to (B, T, nh, C // nh)
+        # transpose(1, 2) swaps the T and
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
 
+        # Operations
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        
+        # Autoreggrssive mask, assures causal self attention
         att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        
+        # Make sure it sums to one
         att = F.softmax(att, dim=-1)
         y = att @ v
-        
-        
-        
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
 
+        y = self.c_proj(y)
+        return y
+        
 class MLP(nn.Module):
 
     def __init__(self, config):
@@ -67,8 +92,8 @@ class Block(nn.module):
 
     def forward(self, x):
         # The forward method controls the flow through the neural network
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.mlp(self.ln_2(x))
+        x = x + self.attn(self.ln_1(x)) # Talking to eachother
+        x = x + self.mlp(self.ln_2(x)) # Thinking by themselves
         return x
 
 @dataclass
@@ -92,7 +117,7 @@ class GPT(nn.module):
         # ln_f (layer normal final)
         # lm_head (language model head, projects to vocab size (50257))
         # no bias, therefore y = mx + 0
-         self.transformer = nn.ModuleDict(dict(
+        self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
             wpe = nn.Embedding(config.block_size, config.n_embd),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),

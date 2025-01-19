3d4a9c2c95827a1522b0986277c0c6c918b9a47d
harryjordan1099 <harry.jordan@hotmail.co.uk>
2025-01-19 15:08:20 +0000

    Finall grad accum

--- pytorch/train_gpt2.py
+++ pytorch/train_gpt2.py
@@ -282,16 +282,17 @@ class GPT(nn.Module):
         num_decay_params = sum(p.numel() for p in decay_params)
         num_nodecay_params = sum(p.numel() for p in nodecay_params)
         print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,}  parameters")
-        print(f"num non-decayed parameter tensors: {len(num_nodecay_params)}, with {num_nodecay_params:,}  parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,}  parameters")
         # Create AdamW optimizer and use the fused version if it is available
         fused_available = False
 
         # python 3.12 hack, kernel fussion for AdamW
-        try:
-            torch.optim.AdamW([], fused=True)  # Test with an empty parameter list
-            fused_available = True
-        except TypeError:
-            fused_available = False
+        # try:
+        #     torch.optim.AdamW([], fused=True)  # Test with an empty parameter list
+        #     fused_available = True
+        # except TypeError:
+        #     fused_available = False
+        fused_available = True
         used_fused = fused_available and "cuda" in device
         print(f"using fused AdamW: {used_fused}")
         optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=used_fused)
@@ -344,6 +345,14 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
 
+total_batch_size = 524288 # 2**19, roughly 0.5 M of tokens
+B = 16 # micro batch size
+T = 1024 # sequence length
+assert total_batch_size // (B * T) # make sure you can divide the total_batch_size by B*T
+grad_accum_steps = total_batch_size // (B * T)
+print(f"total desired batch size: {total_batch_size}")
+print(f"=> calculated gradient accumation steps: {grad_accum_steps}")
+
 train_loader = DataLoader(B=8, T=1024)
 
 # high = tf_32, setting all matrix multipication to utlize tf32 precision
@@ -382,16 +391,20 @@ optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, dev
 
 for step in range(max_steps):
     t0 = time.time()
-    # Create batch
-    x, y = train_loader.next_batch()
-    x, y = x.to(device), y.to(device)
     
     # always start with a zero gradient
     optimizer.zero_grad()
-    # cast some operations to BF16, otherwise in FP32, as some operations are effected more.
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
-        logits, loss = model(x, y)
-    loss.backward() # accumulates gradient
+    # Create batch
+    loss_accum = 0.0
+    for micro_step in range(grad_accum_steps):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        # cast some operations to BF16, otherwise in FP32, as some operations are effected more.
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
+        loss = loss / grad_accum_steps
+        loss_accum += loss.detach()
+        loss.backward() # accumulates gradient
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) # stop model shock, limit gradient to 1.0.
     lr = get_lr(step)
     for param_group in optimizer.param_groups:
@@ -404,9 +417,9 @@ for step in range(max_steps):
     dt = (t1 - t0)*1000
 
     # token count
-    tokens_processed = train_loader.B * train_loader.T
+    tokens_processed = train_loader.B * train_loader.T * grad_accum_steps
     tokens_per_sec = tokens_processed / dt
-    print(f"step {step} | loss: {loss.item():.6f} | norm: {norm:.4f} | dt: {dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
+    print(f"step {step} | loss: {loss_accum.item():.6f} | norm: {norm:.4f} | dt: {dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 

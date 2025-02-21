aa66aff4434d62794e33a39d6223cb173c811555
HarryJordan <>
2025-02-21 13:29:23 +0000

    Adding multiheadattention

--- tinystories/build_a_large_language_model/chapter-3.py
+++ tinystories/build_a_large_language_model/chapter-3.py
@@ -410,4 +410,57 @@ print("context_vecs.shape:", context_vecs.shape)
 # %%
 context_length
 
+
+# %% [markdown]
+# ## 3.6.2 Implementing multi-head attention with weight splits
+
 # %%
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out,
+                context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), \
+            "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length),
+                       diagonal=1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        
+        queries = self.W_query(x)
+        keys = self.W_key(x)
+        values = self.W_value(x)
+
+        # Split matrix with num_heads
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # (b, num_tokens, num_heads, head_dim) -> (b, num_heads, num_tokens, head_dim)
+        queries = queries.transpose(1, 2)
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        # create mask
+        attn_scores = queries @ keys.transpose(2, 3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax
+        
+        
+
+        
